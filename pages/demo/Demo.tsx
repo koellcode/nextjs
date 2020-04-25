b0c73ba0b5ee8e017492ac0e0d8341d601e6d6bd
Thomas Kölling <thomas@Thomass-Air.fritz.box>
2020-04-25 10:33:00 +0200

    playing around with TSX

---
+++ pages/demo/Demo.tsx
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+export interface HelloProps {
+  compiler: string;
+  framework: string;
+}
+
+export const Hello = ({ compiler, framework }: HelloProps) => (
+  <h1>
+    Hello from {compiler} and {framework}!
+  </h1>
+);
